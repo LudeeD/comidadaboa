a8a23a78d419184293eb1a0ca0a1a661816b4300
Luís Silva <luiscomsnofim@gmail.com>
2023-03-26 21:39:25 +0200

    feat: MVP

---
+++ docs/_app/immutable/chunks/1.b85c377a.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.74e876ca.js";export{t as component};
--- docs/_app/immutable/chunks/1.cf570abf.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.31cfe78e.js";export{t as component};
