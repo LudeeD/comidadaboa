59694fb008cbccba807160c5bcf63e7c410982b0
Luís Silva <luiscomsnofim@gmail.com>
2023-09-21 19:34:10 +0200

    feat: using zola

--- docs/_app/immutable/chunks/1.c4e3f576.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.f93d4d79.js";export{t as component};
--- docs/_app/immutable/chunks/4.ce36fdc6.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/menu-page.svelte.679fd66d.js";export{t as component};
