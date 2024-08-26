8264e5f33a378918cb1ca1a4b236c570892051eb
Luís Silva <luiscomsnofim@gmail.com>
2024-08-26 22:24:04 +0200

    feat: start using the wasm version to have support for extensions

--- app/recipes/[slug]/page.js
+++ app/recipes/[slug]/page.js
@@ -17,11 +17,38 @@ export const generateMetadata = async ({ params, searchParams }) => {
   };
 };
 
-export default function RecipePage(props) {
+export default async function RecipePage(props) {
   const { params } = props;
   const { slug } = params;
 
-  const { recipe, image } = getPostContent(slug);
+  const { recipe, image } = await getPostContent(slug);
+
+  const ingredients = recipe.ingredients;
+
+  const formatQuantity = (quantity) => {
+    if (!quantity || typeof quantity !== "object") return "";
+
+    let value = "";
+    let unit = quantity.unit || "";
+
+    if (quantity.value && quantity.value.type === "fixed") {
+      const fixedValue = quantity.value.value;
+      if (fixedValue && fixedValue.type === "number") {
+        const numberValue = fixedValue.value;
+        if (numberValue && numberValue.type === "regular") {
+          value = numberValue.value;
+        }
+      }
+    }
+
+    // Format the value (assuming it's a number)
+    const formattedValue = Number(value).toLocaleString(undefined, {
+      minimumFractionDigits: 0,
+      maximumFractionDigits: 2,
+    });
+
+    return `${formattedValue} ${unit} `;
+  };
 
   // return if exists
   const MyImage = ({ slug }) => {
@@ -52,44 +79,61 @@ export default function RecipePage(props) {
           </span>
         );
       case "ingredient":
-        return <span className="font-medium text-green-600">{part.name}</span>;
+        let ingredient = ingredients[part.index];
+        return (
+          <span className="font-medium text-green-600">{ingredient.name}</span>
+        );
       case "cookware":
-        return <span className="font-medium text-blue-600">{part.name}</span>;
+        return <span className="font-medium text-blue-600">{part.value}</span>;
       default:
         console.log("Unknown part type", part);
         return null;
     }
   };
 
-  return (
-    <main className="bg-red text-lg flex flex-col gap-4">
-      <h1 className="text-2xl text-center font-bold">
-        {recipe.metadata.title}
-      </h1>
-      <div className="bg-white rounded-md px-4 py-2">
-        <h3 className="mb-2 font-semibold text-lg">Ingredientes</h3>
-        <ul className="list-disc list-inside">
-          {recipe.ingredients.map((ingredient, index) => (
-            <li key={index}>
-              {ingredient.quantity == "some" ? "" : ingredient.quantity}{" "}
-              {ingredient.units} {ingredient.name}
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div className="bg-white rounded-md px-4 py-2">
-        <h3 className="mb-4 font-semibold text-lg">Instruções</h3>
+  const Section = ({ title, steps }) => {
+    return (
+      <>
+        <h3 className="font-semibold text-lg">{title}</h3>
         <ol className="list-decimal list-inside">
-          {recipe.steps.map((step, index) => (
+          {steps.map((step, index) => (
             <li key={index} className="mb-2">
-              {Array.isArray(step)
-                ? step.map((part, partIndex) => (
+              {Array.isArray(step.value.items)
+                ? step.value.items.map((part, partIndex) => (
                     <Step key={partIndex} part={part} />
                   ))
                 : step}
             </li>
           ))}
         </ol>
+      </>
+    );
+  };
+
+  return (
+    <main className="bg-red text-lg flex flex-col gap-4">
+      <h1 className="text-2xl text-center font-bold">
+        {recipe.metadata.map.title}
+      </h1>
+      <div className="bg-white rounded-md px-4 py-2">
+        <h3 className="mb-2 font-semibold text-lg">Ingredientes</h3>
+        <ul className="list-disc list-inside">
+          {recipe.ingredients.map((ingredient, index) => (
+            <li key={index}>
+              {ingredient.quantity ? formatQuantity(ingredient.quantity) : ""}
+              {ingredient.name}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="bg-white rounded-md px-4 py-2">
+        <h3 className="mb-4 font-semibold text-lg">Instruções</h3>
+        {recipe.sections.map((section, index) => {
+          //return JSON.stringify(section);
+          return (
+            <Section key={index} title={section.name} steps={section.content} />
+          );
+        })}
       </div>
 
       <MyImage slug={slug} />
--- utils/getPostContent.js
+++ utils/getPostContent.js
@@ -1,20 +1,44 @@
 import fs from "fs";
-import { Recipe } from "@cooklang/cooklang-ts";
+import path from "path";
+import { fileURLToPath } from "url";
 
-export default function getPostContent(slug) {
-  const path = `${process.cwd()}/recipes/${slug}.cook`;
-  const rawContent = fs.readFileSync(path, {
-    encoding: "utf-8",
-  });
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-  const path_image = `${process.cwd()}/public/images/${slug}.jpg`;
+// Import the JS file from the pkg directory
+import initWasm, * as wasm from "../cooklang/cooklang_playground.js";
 
-  let image = fs.existsSync(path_image);
+let initialized = false;
 
-  const ret = {
-    recipe: new Recipe(rawContent),
-    image: image,
-  };
+export default async function getPostContent(slug) {
+  if (!initialized) {
+    // Initialize the WASM module
+    const wasmPath = path.join(
+      __dirname,
+      "..",
+      "cooklang",
+      "cooklang_playground_bg.wasm",
+    );
+    await initWasm(fs.readFileSync(wasmPath));
+    initialized = true;
+  }
 
-  return ret;
+  // Now you can use the imported functions
+  const state = new wasm.State();
+  console.log("CookLang version:", wasm.version());
+
+  const filePath = path.join(process.cwd(), "recipes", `${slug}.cook`);
+  const filePathImage = path.join(
+    process.cwd(),
+    "public",
+    "images",
+    `${slug}.jpg`,
+  );
+  const rawContent = fs.readFileSync(filePath, "utf-8");
+
+  const { value, error } = state.parse_full(rawContent, true);
+
+  console.log("Errors:", error);
+
+  return { recipe: JSON.parse(value), image: fs.existsSync(filePathImage) };
 }
