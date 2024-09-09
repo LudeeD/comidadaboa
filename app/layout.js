79a56540b5988d8fd574029938717955a41ec6fb
Luís Silva <luiscomsnofim@gmail.com>
2024-09-09 23:51:38 +0200

    update: add analytics

--- app/layout.js
+++ app/layout.js
@@ -59,6 +59,7 @@ export default function RootLayout({ children }) {
           rel="icon"
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🧑‍🍳</text></svg>"
         />
+        <script defer data-domain="comidadaboa.com" src="https://analytics.luissilva.eu/js/script.js"></script>
       </head>
       <body className="max-w-3xl mx-auto p-4 min-h-screen flex flex-col">
         <Title />
